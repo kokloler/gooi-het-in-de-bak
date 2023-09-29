bac3907a33c5e2b9440bdd64af558f607e18a259
Tim019283 <tim.neto.rodrigues@student.gildeopleidingen.nl>
2023-09-29 11:49:12 +0200

    Update menu-script.js

--- scripts/menu-script.js
+++ scripts/menu-script.js
@@ -2,12 +2,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const checkbox = document.getElementById('click');
     const body = document.body;
+    const nav = document.querySelector('nav');
 
     checkbox.addEventListener('change', function() {
         if (this.checked) {
-            body.classList.add('no-scroll');
+            body.classList.add('no-scroll'); // Voeg de "no-scroll" klasse toe
+            window.scrollTo(0, 0); // Scroll naar bovenaan de pagina
         } else {
+            body.classList.remove('no-scroll'); // Verwijder de "no-scroll" klasse
+        }
+    });
+
+    // Voeg een eventlistener toe aan de "logo" om naar boven te scrollen wanneer erop wordt geklikt
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', function() {
+        if (checkbox.checked) {
+            checkbox.checked = false;
             body.classList.remove('no-scroll');
         }
+        window.scrollTo(0, 0); // Scroll naar bovenaan de pagina
     });
 });
