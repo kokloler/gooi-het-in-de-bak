f4dafa40953c88d981c74611b0c83d6d23e87056
Tim019283 <tim.neto.rodrigues@student.gildeopleidingen.nl>
2023-10-02 13:21:38 +0200

    begin gemaakt van faq pagina

---
+++ faq/faq.js
@@ -0,0 +1,8 @@
+const questions = document.querySelectorAll('.question');
+
+        questions.forEach(question => {
+            question.addEventListener('click', () => {
+                const answer = question.nextElementSibling;
+                answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+            });
+        });
