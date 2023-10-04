ce26e461c0d02782b8dc92db4deaa167d00f4f57
Tim019283 <tim.neto.rodrigues@student.gildeopleidingen.nl>
2023-10-04 09:26:02 +0200

    html/css voor formulier

---
+++ faq/faq_form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="..//images/form.ico">
+    <title>Stel een vraag</title>
+    <link rel="stylesheet" type="text/css" href="..//style.css">
+    <link rel="stylesheet" type="text/css" href="..//faq/faq_form.css">
+</head>
+
+<body class="no-hamburger-menu">
+    <nav>
+        <div class="logo">Gooi het in de bak</div>
+        <ul>
+            <li><a href="../faq/faq.html">Terug</a></li>
+        </ul>
+    </nav>
+
+    <main id="main_reg">
+        <h1>Stel een vraag</h1>
+        <div class="container">
+            <form action="formulier.php" method="post">
+                <label for="naam">Naam:</label>
+                <input type="text" id="naam" name="naam" required>
+
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="vraag">Vraag:</label>
+                <textarea id="vraag" name="vraag" rows="4" required></textarea>
+
+                <input type="submit" value="Verstuur*" id="reg_button">
+            </form>
+            <p>* Alle inzendingen worden anoniem verwerkt.</p>
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer">&copy; 2023 Afvalrecycling </div>
+    </footer> 
+
+</body>
+</html>
