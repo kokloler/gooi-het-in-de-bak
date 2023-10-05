cbf84aa7fb29fe4234a7af7334f53ee7e2cee963
Tim019283 <tim.neto.rodrigues@student.gildeopleidingen.nl>
2023-10-05 10:09:11 +0200

    Toevoegen van php om ingediende vragen te kunnen zien

--- medewerker/medewerker.php
+++ medewerker/medewerker.php
@@ -32,13 +32,57 @@ mysqli_close($conn);
 </head>
 <body>
 <nav>
-        <div class="logo">Gooi het in de bak</div>
+        <div class="logo">Medewerkers pagina</div>
         <ul>
             <li class="nav-item">
             <li><a href="logout.php" class="logout-btn"><i class="fas fa-door-open"></i></a></li>
             </li>
         </ul>
     </nav>
-    Hallo world!
+
+    <div class="table-container">
+        <h2>Ingediende vragen</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>E-mail</th>
+                    <th>Vraag</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    // Verbinding maken met de database
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "faq";
+                    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                    }
+
+                    $sql = "SELECT naam, email, vraag FROM form";
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>".$row['naam']."</td>";
+                            echo "<td>".$row['email']."</td>";
+                            echo "<td>".$row['vraag']."</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='3'>Geen gegevens beschikbaar</td></tr>";
+                    }
+
+                    mysqli_close($conn);
+                ?>
+            </tbody>
+        </table>
+    </div>
+
 </body>
 </html>
